bcd8dd8a11f343b2222dfb6ee481ef8c9befdf55
its-hafsa04 <139751784+its-hafsa04@users.noreply.github.com>
2024-01-21 17:57:18 +0500

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,36 @@
-import express from "express";
+/*import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
+dotenv.config();
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("server connected to mongodb");
+}).catch((err)=>{
+    console.log(err)}
+    );
 
 const app = express();
 
 app.listen(3000 ,() =>{
-    console.log("server is running");
-})
+    console.log("server is running on port 3000");
+})*/
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+// Load environment variables from .env file
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("server connected to mongodb");
+}).catch((err) => {
+    console.log(err);
+});
+
+const app = express();
+
+app.listen(3000, () => {
+    console.log("server is running on port 3000");
+});
