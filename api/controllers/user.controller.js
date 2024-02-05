4f6f22c646fd33f8fb7bf5f8b734a7eccba0eeb2
its-hafsa04 <139751784+its-hafsa04@users.noreply.github.com>
2024-02-05 22:09:25 +0500

    add user delete functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -29,3 +29,17 @@ res.status(201).json(rest);
 }
 
 }
+
+export const deleteUser = async (req ,res ,next, error) =>{
+    if (req.user.id !== req.params.id) return next(errorHandler(401, 
+'You can only delete your own account!'));
+try {
+    
+    await User.findByIdAndDelete(req.params.id);
+    res.clearCookie('access_token');
+    res.status(200).json('User has deleted!');
+
+} catch (error) {
+    next(error)
+}
+}
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { useRef } from 'react'
 import { getStorage, ref, getDownloadURL, uploadBytesResumable} from 'firebase/storage'
-import { updateUserStart, updateUserSuccess, updateUserFailure } from '../redux/user/userSlice.js';
+import { updateUserStart, updateUserSuccess, updateUserFailure,deleteUserFailure,
+  deleteUserStart, deleteUserSuccess } from '../redux/user/userSlice.js';
 import {app} from '../firebase'
 import { useDispatch } from 'react-redux';
 
@@ -70,6 +71,24 @@ const handleFileUpload = (file) =>{
       }
     };
 
+    const handleDeleteUser = async () => {
+      try {
+        dispatch(deleteUserStart());
+        const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+          method: 'DELETE',
+        });
+        const data = await res.json();
+        if (data.success === false) {
+          dispatch(deleteUserFailure(data.message));
+          return;
+        }
+        dispatch(deleteUserSuccess(data));
+      } catch (error) {
+        dispatch(deleteUserFailure(error.message));
+      }
+    };
+  
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -105,7 +124,7 @@ const handleFileUpload = (file) =>{
         </form>
 
         <div className='flex justify-between mt-5'>
-        <span className='text-red-700 cursor-pointer'>Delete account</span>
+        <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>Delete account</span>
         <span className='text-red-700 cursor-pointer'>Sign out</span>
       </div>
       <p className='text-red-700 mt-5'>{error ? error : ''}</p>
