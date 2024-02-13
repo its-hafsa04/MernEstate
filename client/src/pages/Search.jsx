c73a7052ba7cb8f786fcdebacecb8e06fd6df79c
its-hafsa04 <139751784+its-hafsa04@users.noreply.github.com>
2024-02-13 13:53:29 +0500

    add show more listings functionality

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -17,6 +17,7 @@ export default function Search() {
 
       const [loading, setLoading] = useState(false);
       const [listings, setListings] = useState([]);
+      const [showMore, setShowMore] = useState(false);
 
       useEffect ( () => {
         const urlParams = new URLSearchParams(location.search);
@@ -47,6 +48,11 @@ export default function Search() {
             const searchQuery = urlParams.toString();
             const res = await fetch(`/api/listing/get?${searchQuery}`);
             const data = await res.json();
+            if (data.length > 8) {
+        setShowMore(true);
+      } else {
+        setShowMore(false);
+      }
             setListings(data);
             setLoading(false);
           };
@@ -94,6 +100,19 @@ export default function Search() {
         navigate(`/search?${searchQuery}`);
       };
     
+const onShowMoreClick = async () => {
+    const numberOfListings = listings.length;
+    const startIndex = numberOfListings;
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set('startIndex', startIndex);
+    const searchQuery = urlParams.toString();
+    const res = await fetch(`/api/listing/get?${searchQuery}`);
+    const data = await res.json();
+    if (data.length < 9) {
+      setShowMore(false);
+    }
+    setListings([...listings, ...data]);
+  };
 
   return (
     <div className='flex flex-col md:flex-row'>
@@ -173,6 +192,12 @@ export default function Search() {
           {!loading && listings && listings.map((listing) => (
               <ListingItem key={listing._id} listing={listing} />
             ))}
+            {showMore && (
+            <button onClick={onShowMoreClick} className='text-green-700 hover:underline p-7
+             text-center w-full'>
+              Show more
+            </button>
+          )}
         </div>
       </div>
     </div>
