950cde96478cd091cdb7fc9a94fc452e15d393f9
its-hafsa04 <139751784+its-hafsa04@users.noreply.github.com>
2024-02-18 20:39:41 +0500

    vercel config

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://mern-estate-api-cyan.vercel.app',
+        target: 'https://mern-estate-api-cyan.vercel.app',
         changeOrigin: true,
         secure: false,
       },
