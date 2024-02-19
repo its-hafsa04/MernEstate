43a9d3600d60db35c9f11a7daddd73f349f6729b
its-hafsa04 <139751784+its-hafsa04@users.noreply.github.com>
2024-02-19 22:51:42 +0500

    update code

--- api/index.js
+++ api/index.js
@@ -5,7 +5,6 @@ import authRoute from "./routes/auth.route.js"
 import listingRoute from './routes/listing.route.js'
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import path from 'path';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -13,8 +12,6 @@ mongoose.connect(process.env.MONGO).then(() => {
 }).catch((err) => {
     console.log(err);
 });
-
-const __dirname = path.resolve();
 
 const app = express();
 app.use(express.json());
@@ -27,12 +24,6 @@ app.listen(3000, () => {
 app.use("/api/user", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/listing", listingRoute);
-
-app.use(express.static(path.join(__dirname, '/client/dist')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-})
 
 //middleware
 app.use((err, req, res, next) =>{
